c90597a898bb85a9bb83c9d9f76f7045a5006289
eslota <ewas4852@gmail.com>
2024-04-13 15:03:30 +0200

    Zjazd 2 dzień 1

--- src/main/java/pl/edu/wszib/http2/controller/ToDoController.java
+++ src/main/java/pl/edu/wszib/http2/controller/ToDoController.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.edu.wszib.http2.service.ToDoService;
 import pl.edu.wszib.http2.service.model.ToDo;
 import pl.edu.wszib.http2.service.model.ToDoStatus;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -33,5 +33,18 @@ public class ToDoController {
         }
         model.addAttribute("todos", toDoList);
         return "todo/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("newTodo", new ToDo());
+        return "todo/create";
+    }
+
+    @PostMapping("/create")
+    public String createAction(ToDo newTodo, Model model){
+        newTodo.setStatus(ToDoStatus.NEW);
+        toDoService.create(newTodo);
+        return "redirect:/todos";
     }
 }
