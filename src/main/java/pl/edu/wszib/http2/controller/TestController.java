9cd2e7090789a381ff7cbe6fd0cf65286b3952af
eslota <ewas4852@gmail.com>
2024-04-13 11:44:43 +0200

    Zjazd 2 dzień 1

--- src/main/java/pl/edu/wszib/http2/controller/TestController.java
+++ src/main/java/pl/edu/wszib/http2/controller/TestController.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping
     public String test(Model model) {
+        model.addAttribute("imie", "Mateusz");
+        model.addAttribute("pokaz", false);
+        model.addAttribute("kolory",
+                Arrays.asList("Niebieski", "Żółty", "Czerwony"));
+        model.addAttribute("teraz", new Date());
         return "test";
     }
 }
